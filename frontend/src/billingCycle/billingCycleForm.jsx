6150d64e019f8bdef5b3a4d0ee39b2eac876d0fe
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-22 12:22:44 -0300

    modified function and components to display actual data in the credit ist table

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { reduxForm, Field } from 'redux-form'
+import { reduxForm, Field, formValueSelector } from 'redux-form'
 
 import { init } from './billingCycleActions'
 import LabelAndInput from '../common/form/labelAndInput'
@@ -19,7 +19,7 @@ class BillingCycleForm extends Component {
                     label="Mês" cols="12 4" placeholder="Informe o mês" />
                     <Field name="year" component={LabelAndInput} readOnly={readOnly}
                     label="Ano" cols="12 4" placeholder="Informe o ano" />
-                    <CreditList cols="12 6" readOnly={readOnly}/>
+                    <CreditList cols="12 6" list={this.props.credits} readOnly={readOnly}/>
                 </div>
                 <div className="box-footer">
                     <button type="submit" className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
@@ -33,6 +33,10 @@ class BillingCycleForm extends Component {
 
 BillingCycleForm = reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm)
 
+const selector = formValueSelector('billingCycleForm')
+
+const mapStateToProps = state => ({credits: selector(state, 'credits')})
+
 const mapDispatchToProps = dispatch => bindActionCreators({init}, dispatch)
 
-export default connect(null, mapDispatchToProps)(BillingCycleForm)
+export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleForm)
