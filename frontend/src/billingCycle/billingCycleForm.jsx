ef78d7dd2adb67bd92ff1234d9142e500a92fbbb
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-22 12:53:10 -0300

    created summay component.

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -6,6 +6,7 @@ import { reduxForm, Field, formValueSelector } from 'redux-form'
 import { init } from './billingCycleActions'
 import LabelAndInput from '../common/form/labelAndInput'
 import ItemList from './itemList'
+import Summary from './summary'
 
 class BillingCycleForm extends Component {
     render() {
@@ -19,6 +20,7 @@ class BillingCycleForm extends Component {
                     label="Mês" cols="12 4" placeholder="Informe o mês" />
                     <Field name="year" component={LabelAndInput} readOnly={readOnly}
                     label="Ano" cols="12 4" placeholder="Informe o ano" />
+                    <Summary credit={1000} debt={100}/>
                     <ItemList cols="12 6" list={this.props.credits} readOnly={readOnly} field="credits" legend="Créditos"/>
                     <ItemList cols="12 6" list={this.props.debts} readOnly={readOnly} field="debts" legend="Débitos" showStatus={true}/>
                 </div>
