c160b65b0a67ee748774c63c18211415efa80bfa
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-23 11:00:55 -0300

    changed port.

--- frontend/src/dashboard2/dashboard2.jsx
+++ frontend/src/dashboard2/dashboard2.jsx
@@ -6,7 +6,7 @@ import Content from '../common/template/content'
 import ValueBox from '../common/widget/valueBox'
 import Row from '../common/layout/row'
 
-const BASE_URL = 'http://localhost:3003/api'
+const BASE_URL = 'http://localhost:3004/api'
 
 export default class Dashboard2 extends Component {
     constructor(props) {
